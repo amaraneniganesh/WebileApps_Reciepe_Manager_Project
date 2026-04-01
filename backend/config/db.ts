2a0d2c3d2c1045c0e1bc8881d222f3ad19607b72
madhusankark <2200032935cseh@gmail.com>
2026-04-01 18:10:39 +0530

    Update db.ts

--- backend/config/db.ts
+++ backend/config/db.ts
@@ -1,1 +1,17 @@
+import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
+const dbName = process.env.DB_NAME || 'recipe_manager_db';
+const dbUser = process.env.DB_USER || 'root';
+const dbPasword = process.env.DB_PASSWORD || 'm115@224A';
+const dbHost = process.env.DB_HOST || 'localhost';
+ export const sequelize  = new Sequelize(
+    dbName,
+    dbPasword,
+    dbHost,{
+        host:dbHost,
+        dialect:"mysql",
+        logging:false
+    }
 
+ );
